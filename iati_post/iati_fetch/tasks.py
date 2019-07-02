99d7f50bb177b386b3976decf4aca1a3b3ca2e7b
Joshua Brooks <josh.vdbroek@gmail.com>
2019-07-03 01:15:31 +0800

    xml_requests_process works nicely

--- iati_post/iati_fetch/tasks.py
+++ iati_post/iati_fetch/tasks.py
@@ -15,33 +15,47 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-async def xml_requests_list(organisations: list = None, session:ClientSession = None) -> List[requesters.XMLRequest]:
+async def fetch_requests(*requests, semaphore_count=2000):
+    sem = asyncio.Semaphore(semaphore_count)
+    tasks = []
+    async with ClientSession(connector=TCPConnector(ssl=False)) as session:
+        for request in requests:
+            # cached = request.is_cached()
+            # if (cached and exclude_cached) or (not cached and exclude_uncached):
+            #     logger.debug('Not included: %s', request)
+            #     continue
+            tasks.append(request.bound_get(sem, session=session))
+        task_count = len(tasks)
+        print(f"Gathering {task_count} tasks")
+        await asyncio.gather(*tasks)
+
+
+async def organisation_requests_list(organisation_abbreviations: List[str]) -> List[requesters.OrganisationRequestDetail]:
+    return [requesters.OrganisationRequestDetail(organisation_handle=abbr) for abbr in organisation_abbreviations]
+
+async def organisation_requests_fetch(organisations: List[requesters.OrganisationRequestDetail]) -> None:
+    await fetch_requests(*organisations)
+
+async def xml_requests_list(organisations: List[requesters.OrganisationRequestDetail]) -> List[requesters.XMLRequest]:
     """
     Return a list of all of the XML requests associated
     with an organisation  abbreviation
     """
     requests_list = []
-
-    if not session:
-        async with ClientSession(connector=TCPConnector(ssl=False)) as session:
-            return await xml_requests_list(organisations, session)
-
-    if not organisations:
-        orl = requesters.OrganisationRequestList()
-        organisations = await orl.to_list()
-
-    for abbr in organisations:
-        detail_request = requesters.OrganisationRequestDetail(organisation_handle=abbr)
-        xml_requests = await detail_request.iati_xml_requests(session=session)
-        for xml_request in xml_requests:
-            assert xml_request.organisation_handle
-            requests_list.append(xml_request)
+    async with ClientSession(connector=TCPConnector(ssl=False)) as session:
+        for detail_request in organisations:
+            xml_requests = await detail_request.iati_xml_requests(session=session)
+            for xml_request in xml_requests:
+                assert xml_request.organisation_handle
+                requests_list.append(xml_request)
 
     return requests_list
 
+async def xml_requests_fetch(requests_list: List[requesters.IatiXMLRequest]) -> None:
+    await fetch_requests(*requests_list)
 
 async def xml_requests_get(
-    organisations: list = None, exclude_uncached=False, exclude_cached=False
+    organisations: List[str] = None
 ) -> List[requesters.IatiXMLRequest]:
     """
     Fetches all of the XML requests associated with particular organisations
@@ -50,30 +64,19 @@ async def xml_requests_get(
         orl = requesters.OrganisationRequestList()
         organisations = await orl.to_list()
 
-    assert isinstance(organisations, list)
-    # Limit parallel requests avoiding an OSError: too many open files
-    sem = asyncio.Semaphore(2000)
-    tasks = []
-    async with ClientSession(connector=TCPConnector(ssl=False)) as session:
-        xml_requests = await xml_requests_list(organisations, session)
-
-        for request in xml_requests:
-            cached = request.is_cached()
-            if (cached and exclude_cached) or (not cached and exclude_uncached):
-                logger.debug('Not included: %s', request)
-                continue
-            tasks.append(request.bound_get(sem, session=session))
-        task_count = len(tasks)
-        print(f"Gathering {task_count} tasks")
-        await asyncio.gather(*tasks)
+    organisation_requests = await organisation_requests_list(organisations)
+    # await organisation_requests_fetch(organisation_requests)
+    xml_requests = await xml_requests_list(organisation_requests)
+    # await xml_requests_fetch(xml_requests)
+    await xml_requests_fetch(xml_requests)
     return xml_requests
 
 
 async def xml_requests_process(
-    organisations: list = None, exclude_cached: bool = False, include_activities=True, include_organisations=True
+    organisations: list = None, include_activities=True, include_organisations=True
 ):
     # Collect & cache all of the Organisation information from IATI
-    xml_requests = await xml_requests_get(organisations, exclude_cached)
+    xml_requests = await xml_requests_get(organisations)
     for req in xml_requests:
         
         # Search for tags in the JSON-dumped data
